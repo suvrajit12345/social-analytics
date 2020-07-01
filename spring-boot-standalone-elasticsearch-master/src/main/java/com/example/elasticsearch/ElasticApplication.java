0371752674faf836910a3ff21c3155de366dc9d4
Kirtishree123 <kirtipooja1998@gmail.com>
2020-06-30 19:29:40 -0700

    done

--- spring-boot-standalone-elasticsearch-master/src/main/java/com/example/elasticsearch/ElasticApplication.java
+++ spring-boot-standalone-elasticsearch-master/src/main/java/com/example/elasticsearch/ElasticApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ElasticApplication {
 
-     public static void main(String[] args) {
-				SpringApplication.run(ElasticApplication.class, args);
-			}	
-     
-    
-
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticApplication.class, args);
 	}
+}
