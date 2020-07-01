d24187636fd46bf18be79c78a7b8e4335746d8e4
Kirtishree123 <kirtipooja1998@gmail.com>
2020-06-30 23:26:09 -0700

    done

---
+++ springboot-standalone-elasticsearch-master/src/main/java/com/example/elasticsearch/controller/service.java
@@ -0,0 +1,10 @@
+package com.example.elasticsearch.controller;
+
+public class service {
+
+	public static Boolean analyse(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
