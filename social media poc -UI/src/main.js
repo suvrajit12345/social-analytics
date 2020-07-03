cd1c7177b42080b255f5f2a4bb5668daf9fafa97
shivarepo <shivaprasadsamal97@gmail.com>
2020-07-03 15:04:23 +0530

    updated code

--- social media poc -UI/src/main.js
+++ social media poc -UI/src/main.js
@@ -1,99 +1,131 @@
 import React from 'react';
 import { Map, Tooltip, GeoJSON, TileLayer, DivOverlay } from "react-leaflet";
 import { statesData } from "./data/data.js"
-import { tD } from "./data/index"
+//import { tD } from "./data/index"
 import { searchState } from "./assets/fetchState"
 // postCSS import of Leaflet's CSS
 import 'leaflet/dist/leaflet.css';
+import ElasticSearchData from './elasticSearch';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
-function getColor(d) {
-  return d > 15 ? 'red' :
-    d > 5 ? 'orange' : 'green'
-  // return d > 1000 ? '#800026' :
-  //   d > 500 ? '#BD0026' :
-  //     d > 200 ? '#E31A1C' :
-  //       d > 100 ? '#FC4E2A' :
-  //         d > 50 ? '#FD8D3C' :
-  //           d > 20 ? '#FEB24C' :
-  //             d > 10 ? '#FED976' :
-  //               '#FFEDA0';
-}
-function style(feature) {
-  return {
-    fillColor: getColor(feature.properties.count),
-    weight: 2,
-    opacity: 1,
-    color: 'white',
-    dashArray: '3',
-    fillOpacity: 0.7
-  };
-}
 
-function onEachFeature(feature, layer) {
-  layer.on({
-    onMouseOver: clickToFeature.bind(this)
-  }).bindPopup("State Name: "+feature.properties.name+"</br>"+"Number Of Post: "+feature.properties.count);
-  layer.on({
-    dblclick: clickToFeatureDbl.bind(this)
-  })
-}
-function clickToFeature(e) {
-  var layer = e.target;
-  console.log("I doubleclicked on " + layer.feature.properties.name + ' Id '+ layer.feature.id);
 
-}
-function clickToFeatureDbl(f){
-  console.log('hii');
-  var layer = f.target;
-  window.location = "/aggrid?name="+layer.feature.properties.name;
-  
-}
-function tooltipFn(e){
-  console.log(e);
-}
-function leafLetMap() {
-  let sn = [];
-  for (let i = 0; i < tD.hits.hits.length; i++) {
-    if (tD.hits.hits[i]._source.location) {
-      if (searchState(tD.hits.hits[i]._source.location)) {
-        sn.push(searchState(tD.hits.hits[i]._source.location));
-      }
-    }
-  }
-  for (let j = 0; j < statesData.features.length; j++) {
-    let x = 0;
-    for (let y = 0; y < sn.length; y++) {
-      if (sn[y] == statesData.features[j].properties.name) {
-        statesData.features[j].properties["count"] = x++;
-      }
-    }
-  }
-  return (
-    <div class="container p-3 my-3  text-white">
-      <Map style={{height: '400px',width:'100%'}} center={[37.8, -96]} zoom={4}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <GeoJSON
-          data={statesData}
-          onEachFeature={onEachFeature.bind(this)}
-          style={style.bind(this)}
-        >
-        {/* <Tooltip direction="auto" opacity={1}>{statesData}</Tooltip> */}
-        </GeoJSON>
-      </Map>
-    </div>
-  )
-}
 
 export default class Mapping extends React.Component {
-    render(){
-        return (
-            leafLetMap()
-          );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tD: [],
+      mapLoad: false
     }
- 
+  }
+
+  style(feature) {
+    let d = feature.properties.sentiment;
+    let gc = () => {
+      return d > 1.5 ? 'red' :
+        d > 1 ? 'orange' : 
+        d = 1 ? 'green' :
+        'grey'
+    }
+    return {
+      fillColor: gc(),
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+    };
+  }
+  
+  onEachFeature = (feature, layer) =>{
+    // let goDetail = (feature) =>{
+    //   window.location = "/aggrid?name=" + feature.properties.name;
+    // }
+    layer.on({ onmouseover: this.clicked }).bindPopup("State Name: " + feature.properties.name + "</br>" + "State Sentiment: " + feature.properties.sentiment)
+    // layer.on({
+    //   'dblclick': this.clicked(feature)
+    // });
+    
+  }
+
+  // clicked=(feature)=> {
+  //   console.log(feature.properties.name)
+  // }
+  tooltipFn(e) {
+    console.log(e);
+  }
+
+
+
+  componentWillMount() {
+    var d = new Date(),
+      month = '' + (d.getMonth() + 1),
+      day = '' + (d.getDate() - 1),
+      year = d.getFullYear();
+
+    if (month.length < 2)
+      month = '0' + month;
+    if (day.length < 2)
+      day = '0' + day;
+
+    let today = [year, month, day].join('-');
+    axios.get('http://localhost:8102/rest/users/fetch/date/2020-06-29').then(res => {
+      this.setState({ tD: res.data });
+      let sn = [];
+      for (let i = 0; i < this.state.tD.length; i++) {
+        if (this.state.tD[i].location) {
+          if (searchState(this.state.tD[i].location)) {
+            sn.push({statename: searchState(this.state.tD[i].location), sentimentScore: this.state.tD[i].sentimentScore});
+
+          }
+        }
+      }
+      for (let j = 0; j < statesData.features.length; j++) {
+        let x = 0;
+        statesData.features[j].properties["sentiment"] = [];
+        for (let y = 0; y < sn.length; y++) {
+          if (sn[y].statename == statesData.features[j].properties.name) {
+            statesData.features[j].properties["count"] = x++;
+            statesData.features[j].properties["sentiment"].push(sn[y].sentimentScore);
+          }
+        }
+        var sum=0;
+        if(statesData.features[j].properties["sentiment"]){
+          for(var f=0;f<statesData.features[j].properties["sentiment"].length;f++){
+            sum+=Number(statesData.features[j].properties["sentiment"][f]);
+          }
+          var avg = sum/statesData.features[j].properties["sentiment"].length;
+          statesData.features[j].properties["sentiment"] = avg;
+        }
+      }
+      this.setState({ mapLoad: true });
+    });
+  }
+
+  render() {
+    return this.state.mapLoad
+      ? <div class="container p-3 my-3  text-white">
+        <Map style={{ height: '400px', width: '100%' }} center={[37.8, -96]} zoom={4} >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <GeoJSON
+            data={statesData}
+            onEachFeature={this.onEachFeature.bind(this)}
+            style={this.style}
+          >
+            {/* <Tooltip direction="auto" opacity={1}>{statesData}</Tooltip> */}
+          </GeoJSON>
+
+        </Map>
+
+      </div>
+      : null;
+  }
+
 }
 
- 
